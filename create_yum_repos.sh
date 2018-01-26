97f051a9430a8fa220667b5e2c941dba2312983b
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-01-27 04:05:03 +0900

    fix: アーカイブ作成のエラー修正

--- create_yum_repos.sh
+++ create_yum_repos.sh
@@ -3,9 +3,11 @@ SCRIPT_DIR=$(cd $(dirname $0);pwd)
 
 PACKAGE_NAME=etckeeper
 REPO_NAME=repo_${PACKAGE_NAME}
+ARCH_NAME=${SCRIPT_DIR}/${REPO_NAME}.tar.gz
 
 yum -y install epel-release
 yum -y install ${PACKAGE_NAME} --downloadonly --downloaddir=/tmp/${REPO_NAME}
 yum -y install createrepo
 createrepo --simple-md-filenames /tmp/${REPO_NAME}
-tar czf ${SCRIPT_DIR}/${REPO_NAME}.tar.gz ${REPO_NAME} -C /tmp
+tar czf ${ARCH_NAME} -C /tmp ${REPO_NAME}
+chmod a+w ${ARCH_NAME}
