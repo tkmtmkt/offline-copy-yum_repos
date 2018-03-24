22929fe9522bcf4646aaef1764256af31f54d40f
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-03-24 21:47:33 +0900

    fix: 変数名変更

--- make-archive.sh
+++ make-archive.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
-BASE_DIR=$(dirname $SCRIPT_DIR)
 BASE_NAME=$(basename $SCRIPT_DIR)
+PARENT_DIR=$(dirname $SCRIPT_DIR)
 
-tar czf $BASE_NAME-$(date +%Y%m%d).tar.gz -C $BASE_DIR \
+tar czf $BASE_NAME-$(date +%Y%m%d).tar.gz -C $PARENT_DIR \
     --exclude=$BASE_NAME/$BASE_NAME-*.tar.gz \
     $BASE_NAME
