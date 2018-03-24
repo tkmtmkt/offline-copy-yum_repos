cf7b9aa5724aef008ed195ac460870d1ef03335a
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-03-25 00:35:56 +0900

    fix: コメント追加

--- create_yum_repos.sh
+++ create_yum_repos.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 
-PACKAGE_NAME=etckeeper
+# パッケージ情報設定
+#PACKAGE_NAME=etckeeper
+PACKAGE_NAME=supervisor
 REPO_NAME=repo_${PACKAGE_NAME}
 ARCH_NAME=${SCRIPT_DIR}/${REPO_NAME}.tar.gz
 
+# パッケージダウンロード
 yum -y install epel-release
 yum -y install ${PACKAGE_NAME} --downloadonly --downloaddir=/tmp/${REPO_NAME}
 yum -y install createrepo
+
+# リポジトリ作成
 createrepo --simple-md-filenames /tmp/${REPO_NAME}
+
+# アーカイブ作成
 tar czf ${ARCH_NAME} -C /tmp ${REPO_NAME}
 chmod a+w ${ARCH_NAME}
